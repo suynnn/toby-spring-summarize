d585074ba3dc3a7c309229a809fbea4f7c4bb4cd
suynnn <suy00065@gmail.com>
2023-07-12 11:45:01 +0900

    [feat] 1.3.2 인터페이스의 도입

--- src/main/java/book/tobyspring31/ch1/user/dao/DConnectionMaker.java
+++ src/main/java/book/tobyspring31/ch1/user/dao/DConnectionMaker.java
@@ -8,5 +8,6 @@ public class DConnectionMaker implements ConnectionMaker {
     @Override
     public Connection makeConnection() throws ClassNotFoundException, SQLException {
         // D 사의 독자적인 방법으로 Connection을 생성하는 코드
+        return null;
     }
 }
