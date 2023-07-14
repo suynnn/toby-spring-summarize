396882050b90f039c3d0905912ff0465666ce0ac
suynnn <suy00065@gmail.com>
2023-07-14 14:08:13 +0900

    [feat] 1.5 스프링의 IoC_1.5.1 오브젝트 팩토리를 이용한 스프링 IoC : 스프링 빈 팩토리가 사용할 설정정보 만들기

--- src/main/java/book/tobyspring31/ch1/user/UserDaoTest.java
+++ src/main/java/book/tobyspring31/ch1/user/UserDaoTest.java
@@ -1,13 +1,15 @@
 package book.tobyspring31.ch1.user;
 
 import book.tobyspring31.ch1.user.dao.*;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import java.sql.SQLException;
 
 public class UserDaoTest {
     public static void main(String[] args) throws ClassNotFoundException, SQLException {
-        UserDao userDao = new DaoFactory().userDao();
 
-        // ...
+        ApplicationContext context = new AnnotationConfigApplicationContext(DaoFactory.class);
+        UserDao userDao = context.getBean("userDao", UserDao.class);
     }
 }
--- src/main/java/book/tobyspring31/ch1/user/dao/DaoFactory.java
+++ src/main/java/book/tobyspring31/ch1/user/dao/DaoFactory.java
@@ -2,21 +2,18 @@ package book.tobyspring31.ch1.user.dao;
 
 import book.tobyspring31.ch1.user.dao.connectionmaker.ConnectionMaker;
 import book.tobyspring31.ch1.user.dao.connectionmaker.DConnectionMaker;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class DaoFactory {
 
+    @Bean
     public UserDao userDao() {
         return new UserDao(connectionMaker());
     }
 
-    public AccountDao accountDao() {
-        return new AccountDao(connectionMaker());
-    }
-
-    public MessageDao messageDao() {
-        return new MessageDao(connectionMaker());
-    }
-
+    @Bean
     public ConnectionMaker connectionMaker() {
         return new DConnectionMaker();
     }
