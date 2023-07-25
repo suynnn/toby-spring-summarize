fbf8af9e254c262396bf01041dec65cd365e0973
suynnn <suy00065@gmail.com>
2023-07-25 15:47:28 +0900

    [feat] 2.2 UserDaoTest 개선_2.2.2 테스트의 효율적인 수행과 결과 관리 : 테스트 생성

---
+++ src/test/java/book/tobyspring31/ch2/UserDaoTest.java
@@ -0,0 +1,34 @@
+package book.tobyspring31.ch2;
+
+import book.tobyspring31.ch1.user.dao.UserDao;
+import book.tobyspring31.ch1.user.domain.User;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import java.sql.SQLException;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class UserDaoTest {
+
+
+
+    @Test
+    public void addAndGet() throws SQLException {
+        ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+
+        UserDao dao = context.getBean("userDao", UserDao.class);
+        User user = new User();
+        user.setId("user1");
+        user.setName("유저1");
+        user.setPassword("springno1");
+
+        dao.add(user);
+
+        User user2 = dao.get(user.getId());
+
+        assertThat(user2.getName()).isEqualTo(user.getName());
+        assertThat(user2.getPassword()).isEqualTo(user.getPassword());
+    }
+}
