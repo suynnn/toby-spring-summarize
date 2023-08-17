d13111220b22e2d17174dcfa9b15c6bb86464482
suynnn <suy00065@gmail.com>
2023-08-17 15:22:05 +0900

    [feat] 2.3 개발자를 위한 테스팅 프레임워크 JUnit_2.3.2 테스트 결과의 일관성 : 테스트 일관성 유지

--- src/test/java/book/tobyspring31/ch2/UserDaoTest.java
+++ src/test/java/book/tobyspring31/ch2/UserDaoTest.java
@@ -19,16 +19,22 @@ public class UserDaoTest {
         ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
 
         UserDao dao = context.getBean("userDao", UserDao.class);
+
+        dao.deleteAll();
+        assertThat(dao.getCount()).isEqualTo(0);
+
         User user = new User();
         user.setId("user1");
         user.setName("유저1");
         user.setPassword("springno1");
 
         dao.add(user);
+        assertThat(dao.getCount()).isEqualTo(1);
 
         User user2 = dao.get(user.getId());
 
         assertThat(user2.getName()).isEqualTo(user.getName());
         assertThat(user2.getPassword()).isEqualTo(user.getPassword());
+
     }
 }
